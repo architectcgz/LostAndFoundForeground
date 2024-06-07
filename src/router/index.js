2a742c1bce8071dbe045af595eece2f12fb44db7
czf <837629890@qq.com>
2024-06-07 21:48:31 +0800

    创建AboutUs和ContactUs

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'/home',
+      path:'/',
       name:'home',
       meta:{
         title: '校园失物招领管理'
